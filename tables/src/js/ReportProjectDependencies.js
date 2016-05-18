84bf9ed30e09dffb531fcb0081383d1b8beeea34
Dominik Rose <dominik.rose@leanix.net>
2016-05-18 23:03:14 +0200

    Added Project Report

--- tables/src/js/ReportProjectDependencies.js
+++ tables/src/js/ReportProjectDependencies.js
@@ -45,6 +45,20 @@ var ReportProjectDependencies = (function() {
                                             });
                                         }
                                     }
+                                    services = dependentProject.projectHasServices;
+                                    for (var k = 0; k < services.length; k++) {
+                                        if (services[k].serviceID && fsIndex.index.services[services[k].serviceID]) {
+                                            service = fsIndex.index.services[services[k].serviceID];
+                                            output.push({
+                                                project : list[i].displayName,
+                                                projectId : list[i].ID,
+                                                dependentProject : dependentProject.displayName,
+                                                dependentProjectID : dependentProject.ID,
+                                                service : service.fullName,
+                                                serviceID : service.ID,
+                                            });
+                                        }
+                                    }
                                 }
                             }
                         }
@@ -52,15 +66,44 @@ var ReportProjectDependencies = (function() {
                 }
 
 
-                function link(cell, row) {
+                function linkProject(cell, row) {
                     if (row.projectID)
                         return '<a href="' + that.reportSetup.baseUrl + '/projects/' + row.projectID + '" target="_blank">' + cell + '</a>';
+                }
+
+                function linkResource(cell, row) {
                     if (row.resourceID)
                         return '<a href="' + that.reportSetup.baseUrl + '/resources/' + row.resourceID + '" target="_blank">' + cell + '</a>';
+                }
+                
+                function linkService(cell, row) {
                     if (row.serviceID)
                         return '<a href="' + that.reportSetup.baseUrl + '/services/' + row.serviceID + '" target="_blank">' + cell + '</a>';
+                }
+
+                function linkDependentProject(cell, row) {
+                    if (row.dependentProjectID)
+                        return '<a href="' + that.reportSetup.baseUrl + '/projects/' + row.dependentProjectID + '" target="_blank">' + cell + '</a>';
                     return cell;
                 }
+
+                ReactDOM.render(
+                    <div>
+                        <BootstrapTable data={output} striped={true} hover={true} search={true} exportCSV={true}>
+                            <TableHeaderColumn dataField="id" isKey={true} hidden={true}>ID</TableHeaderColumn>
+                            <TableHeaderColumn dataField="project" dataAlign="left" dataSort={true} dataFormat={linkProject} filter={{type: "TextFilter", placeholder: "Please enter a value"}}>Project</TableHeaderColumn>
+                            <TableHeaderColumn dataField="dependentProject" dataAlign="left" dataSort={true} dataFormat={linkDependentProject} filter={{type: "TextFilter", placeholder: "Please enter a value"}}>Dependent Project</TableHeaderColumn>
+                            <TableHeaderColumn dataField="resource" dataAlign="left" dataSort={true} dataFormat={linkResource} filter={{type: "TextFilter", placeholder: "Please enter a value"}}>IT Component</TableHeaderColumn>
+                            <TableHeaderColumn dataField="service" dataAlign="left" dataSort={true} dataFormat={linkService} filter={{type: "TextFilter", placeholder: "Please enter a value"}}>IT Component</TableHeaderColumn>
+                           </BootstrapTable>
+                    </div>,
+                    document.getElementById("app")
+                );
+            });
+    };
+
+    return ReportProjectDependencies;
+})();
 
                 function linkDependentProject(cell, row) {
                     if (row.dependentProjectID)
