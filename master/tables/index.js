c381f7d0d7966abe5c7fcf22e439f15970323444
Dominik Rose <dominik.rose@leanix.net>
2016-05-18 16:37:14 +0200

    Publish new master version

--- master/tables/index.js
+++ master/tables/index.js
@@ -50659,6 +50659,84 @@ var ReportApplicationFunctionalFit = (function() {
     return ReportApplicationFunctionalFit;
 })();
 
+var ReportApplicationFunctionalFit = (function() {
+    function ReportApplicationFunctionalFit(reportSetup, tagFilter, title) {
+        this.reportSetup = reportSetup;
+        this.tagFilter = tagFilter;
+        this.title = title;
+    }
+
+    ReportApplicationFunctionalFit.prototype.render = function() {
+        var that = this;
+
+        var factSheetPromise = $.get(this.reportSetup.apiBaseUrl + '/factsheets?relations=true&types[]=10&types[]=16&types[]=19&pageSize=-1')
+            .then(function (response) {
+                return response.data;
+            });
+
+        $.when(factSheetPromise)
+            .then(function (data) {
+
+                var fsIndex = new FactSheetIndex(data);
+
+                var list = fsIndex.getSortedList('projects');
+
+                var output = [];
+                for (var i = 0; i < list.length; i++) {
+                    if (!that.tagFilter || list[i].tags.indexOf(that.tagFilter) != -1)  {
+
+                        var dependentProjects = list[i].factSheetHasPredecessors;
+
+                        for (var j = 0; z < dependentProjects.length; j++) {
+                            var tmp = dependentProjects[j];
+                            if (tmp) {
+                                if (tmp.projectID && fsIndex.index.projects[tmp.projectID]) {
+                                    resources = tmp.projectHasResources;
+                                    for (var k = 0; k < resources.length; k++) {
+                                        if (resources[k].resourceID && fsIndex.index.resourcess[resources[k].resourceID]) {
+                                            output.push({
+                                                project : list[i].fullName,
+                                                projectId : list[i].ID,
+                                                dependentProject : tmp.fullName,
+                                                dependentProjectId : tmp.ID,
+                                                resource : resources[k].fullName,
+                                                resourceID : resources[k].ID,
+                                            });
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
+
+                function link(cell, row) {
+                    return '<a href="' + that.reportSetup.baseUrl + '/services/' + row.id + '" target="_blank">' + cell + '</a>';
+                }
+
+                function linkBusinessCapability(cell, row) {
+                    if (row.businessCapabilityID)
+                        return '<a href="' + that.reportSetup.baseUrl + '/businessCapabilities/' + row.businessCapabilityID + '" target="_blank">' + cell + '</a>';
+                }
+
+                ReactDOM.render(
+                    React.createElement("div", null, 
+                        React.createElement(BootstrapTable, {data: output, striped: true, hover: true, search: true, exportCSV: true}, 
+                            React.createElement(TableHeaderColumn, {dataField: "id", isKey: true, hidden: true}, "ID"), 
+                            React.createElement(TableHeaderColumn, {dataField: "project", dataAlign: "left", dataSort: true, dataFormat: linkBusinessCapability, filter: {type: "TextFilter", placeholder: "Please enter a value"}}, "Project"), 
+                            React.createElement(TableHeaderColumn, {dataField: "dependentProject", dataAlign: "left", dataSort: true, dataFormat: linkBusinessCapability, filter: {type: "TextFilter", placeholder: "Please enter a value"}}, "Dependent Project"), 
+                            React.createElement(TableHeaderColumn, {dataField: "resource", dataAlign: "left", dataSort: true, dataFormat: linkBusinessCapability, filter: {type: "TextFilter", placeholder: "Please enter a value"}}, "IT Component")
+                           )
+                    ),
+                    document.getElementById("app")
+                );
+            });
+    };
+
+    return ReportApplicationFunctionalFit;
+})();
+
 (function () {
     'use strict';
 
@@ -50672,6 +50750,9 @@ var ReportApplicationFunctionalFit = (function() {
             break;
         case 'app-functional-fit':
             var report = new ReportApplicationFunctionalFit(reportSetup);
+            break;
+        case 'project-dependencies':
+            var report = new ReportProjectDependencies(reportSetup);
             break;
         case 'process-spend':
             var report = new ReportProcessSpend(reportSetup);
