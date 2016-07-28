b664efd6a909680805eaf2b99e3a0905fd0fdcff
Dominik Rose <dominik.rose@leanix.net>
2016-07-28 19:31:16 +0200

    Publish new master version

--- master/tables/index.js
+++ master/tables/index.js
@@ -49915,7 +49915,7 @@ var ReportCapabilitySpend = (function() {
                     ids.forEach(function(id) {
                         if (fsIndex.index.businessCapabilities[id]) {
                             fsIndex.index.businessCapabilities[id].stats.services.push(service.ID);
-                            fsIndex.index.businessCapabilities[id].stats.serviceCost = serviceCost;
+                            fsIndex.index.businessCapabilities[id].stats.serviceCost += serviceCost;
                         } else {
                             console.warn('Unable to find item with id = ' + id);
                         }
@@ -50766,6 +50766,293 @@ var ReportProjectDependencies = (function() {
     return ReportProjectDependencies;
 })();
 
+var ReportResponsibilities = (function() {
+  function ReportResponsibilities(reportSetup, tagFilter) {
+    this.reportSetup = reportSetup;
+    this.tagFilter = tagFilter;
+  }
+
+  ReportResponsibilities.prototype.render = function() {
+    var that = this;
+
+    var userPromise = $.get(this.reportSetup.apiBaseUrl + '/users')
+    .then(function (response) {
+      return response;
+    });
+    var users = {};
+    $.when(userPromise)
+    .then(function (data) {
+
+      for (var i = 0; i < data.length; i++) {
+        users[data[i].ID] = data[i];
+      }
+    });
+
+    var factSheetPromise = $.get(this.reportSetup.apiBaseUrl + '/factsheets?relations=true&types[]=10&types[]=16&types[]=13&pageSize=-1')
+    .then(function (response) {
+      return response.data;
+    });
+
+    $.when(factSheetPromise)
+    .then(function (data) {
+
+      var fsIndex = new FactSheetIndex(data);
+
+      var list = fsIndex.getSortedList('services');
+
+
+      var getLookup = function(data) {
+        var ret = {};
+        for (var i = 0; i < data.length; i++) {
+          ret[data[i]] = data[i];
+        }
+
+        return ret;
+      };
+
+      var output = [];
+      var markets = {};
+
+      var groupedByMarket = {};
+
+      function getGreenToRed(percent){
+        var r = percent<50 ? 255 : Math.floor(255-(percent*2-100)*255/100);
+        var g = percent>50 ? 255 : Math.floor((percent*2)*255/100);
+        return 'rgb('+r+','+g+',0)';
+      }
+
+
+
+
+      for (var i = 0; i < list.length; i++) {
+
+        var requires = list[i].factSheetHasRequires;
+        var supportTeam = "";
+        var supportTeamID = "";
+        for (var j = 0; j < list[i].factSheetHasRequires.length; j++) {
+          factSheetRefID = list[i].factSheetHasRequires[j].factSheetRefID;
+          provider = fsIndex.index.providers[factSheetRefID];
+          supportTeamID = provider.ID;
+          supportTeam = provider.name + " " + provider.description;
+       }
+
+        
+
+        var subscriptions = {};
+        for (var j = 0; j < list[i].userSubscriptions.length; j++) {
+          userID = list[i].userSubscriptions[j].userID;
+          subscriptions[list[i].userSubscriptions[j].roleDetails]=users[userID].email;
+        }
+
+
+
+        var item = {
+          name : list[i].fullName,
+          type : 'App',
+          id : list[i].ID,
+          completion : Math.floor(list[i].completion * 100),
+          businessResponsible : subscriptions["Business Responsible"],
+          supportTeamID : supportTeamID,
+          supportTeam : supportTeam, 
+          count : ''
+        };
+        output.push(item);
+
+      }
+
+
+
+    function link(cell, row) {
+      if (row.type != 'Market')
+      return '<a href="' + that.reportSetup.baseUrl + '/services/' + row.id + '" target="_blank">' + cell + '</a>';
+      else
+      return '<b>' + cell + '</b>';
+    }
+
+    function linkProvider(cell, row) {
+      return '<a href="' + that.reportSetup.baseUrl + '/providers/' + row.supportTeamID + '" target="_blank">' + cell + '</a>';
+    }
+
+    function percentage(cell, row) {
+      return  '<div class="percentage" style="background-color: ' + getGreenToRed(cell) + ';">' + cell + ' %</div>';
+    }
+
+    function trClassFormat(rowData,rIndex){
+      return 'tr-type-' + rowData.type.toLowerCase();
+    }
+
+
+
+    ReactDOM.render(
+      React.createElement("div", {className: "report-data-quality"}, 
+      React.createElement(BootstrapTable, {data: output, striped: false, hover: true, search: true, condensed: true, exportCSV: true, trClassName: trClassFormat}, 
+      React.createElement(TableHeaderColumn, {dataField: "id", isKey: true, hidden: true}, "ID"), 
+      React.createElement(TableHeaderColumn, {dataField: "name", dataAlign: "left", dataSort: true, dataFormat: link, filter: {type: "TextFilter", placeholder: "Please enter a value"}}, "Application Name"), 
+      React.createElement(TableHeaderColumn, {dataField: "businessResponsible", dataAlign: "left", dataSort: true, filter: {type: "TextFilter", placeholder: "Please enter a value"}}, "Business Responsible"), 
+      React.createElement(TableHeaderColumn, {dataField: "supportTeam", dataAlign: "left", dataSort: true, dataFormat: linkProvider, filter: {type: "TextFilter", placeholder: "Please enter a value"}}, "Support Team")
+      
+      )
+      ),
+      document.getElementById("app")
+    );
+  });
+};
+
+return ReportResponsibilities;
+})();
+
+var ReportApplicationsPerCapability = (function() {
+    function ReportApplicationsPerCapability(reportSetup, tagFilter, title) {
+        this.reportSetup = reportSetup;
+        this.tagFilter = tagFilter;
+        this.title = title;
+    }
+
+    ReportApplicationsPerCapability.prototype.render = function() {
+        var that = this;
+
+        var factSheetPromise = $.get(this.reportSetup.apiBaseUrl + '/factsheets?relations=true&types[]=10&types[]=18&pageSize=-1')
+            .then(function (response) {
+                return response.data;
+            });
+
+        $.when(factSheetPromise)
+            .then(function (data) {
+
+                var fsIndex = new FactSheetIndex(data);
+
+                for (var id in fsIndex.index.businessCapabilities) {
+                    var item = fsIndex.index.businessCapabilities[id];
+                    item['stats'] = {
+                        'services' : [],
+                        'serviceCost' : 0
+                    };
+                    item['serviceCost'] = 0;
+                }
+
+                for (var id in fsIndex.index.services) {
+                    var ids = [];
+                    var service = fsIndex.index.services[id];
+                    if (service.serviceHasBusinessCapabilities && service.serviceHasBusinessCapabilities.length) {
+                        ids = service.serviceHasBusinessCapabilities.map(function(item) {
+                            return item.businessCapabilityID;
+                        });
+                    }
+
+                    var serviceCost = 0;
+                    if (service.serviceHasResources) {
+                        service.serviceHasResources.forEach(function(item) {
+                            if (item.costTotalAnnual)
+                                serviceCost += item.costTotalAnnual;
+                        });
+                    }
+
+                    var serviceHasCriticality = false;
+                    var weightedCriticality = 0; 
+                    if (service.businessCriticalityID) {
+                        serviceHasCriticality = true;
+                        if (service.businessCriticalityID == "4") {
+                            weightedCriticality += 100;
+                        }
+                        if (service.businessCriticalityID == "3") {
+                            weightedCriticality += 66;
+                        }
+                        if (service.businessCriticalityID == "2") {
+                            weightedCriticality += 33;
+                        }
+                    }
+
+                    ids.forEach(function(id) {
+                        if (fsIndex.index.businessCapabilities[id]) {
+                            fsIndex.index.businessCapabilities[id].stats.services.push(service.ID);
+                        
+                            if (serviceHasCriticality) {
+                                if (!fsIndex.index.businessCapabilities[id].stats.servicesWithCriticality) {
+                                    fsIndex.index.businessCapabilities[id].stats.servicesWithCriticality = [];
+                                    fsIndex.index.businessCapabilities[id].stats.weightedCriticality = 0;
+                                }
+
+                                fsIndex.index.businessCapabilities[id].stats.servicesWithCriticality.push(service.ID);
+                                fsIndex.index.businessCapabilities[id].stats.weightedCriticality += weightedCriticality;
+                            }
+
+                        } else {
+                            console.warn('Unable to find item with id = ' + id);
+                        }
+                    });
+
+                }
+
+                // products will be presented by react-bootstrap-table
+
+                var list = fsIndex.getSortedList('businessCapabilities');
+
+                var output = [];
+                for (var i = 0; i < list.length; i++) {
+                    if (!that.tagFilter || list[i].tags.indexOf(that.tagFilter) != -1)  {
+                        var criticality = list[i].stats.servicesWithCriticality ? list[i].stats.weightedCriticality / list[i].stats.servicesWithCriticality.length : 0;
+                        var color = list[i].stats.servicesWithCriticality;
+
+                        var text = "";
+                        if (criticality) {
+                            if (criticality>75) text = "Administrative Service";
+                            else if (criticality>50) text = "Business Operational";
+                            else if (criticality>25) text = "Business Critical";
+                            else text = "Mission Critical";
+                        } else {
+                            if (color) text = "Mission Critical";
+                        }
+                       
+                        output.push({
+                            name : list[i].displayName,
+                            id : list[i].ID,
+                            count : list[i].stats.services.length,
+                            criticality : criticality,
+                            color : color,
+                            text : text
+                        });
+                    }
+                }
+
+                function link(cell, row) {
+                    return '<a href="' + that.reportSetup.baseUrl + '/businessCapabilities/' + row.id + '" target="_blank">' + cell + '</a>';
+                }
+
+                function criticalityFormat(cell, row) {
+                    if (row.color)
+                        return  '<div class="criticality" style="background-color: ' + getGreenToRed(row.criticality) + ';">' + cell + ' </div>';
+                    else
+                        return '<div>' + cell + '</div>';
+                }
+
+                function getGreenToRed(percent){
+                    var r = percent<50 ? 255 : Math.floor(255-(percent*2-100)*255/100);
+                    var g = percent>50 ? 255 : Math.floor((percent*2)*255/100);
+                    return 'rgb('+r+','+g+',0)';
+                }
+
+                var options = {
+                    sortName : 'name',
+                    sortOrder : 'asc'
+                };
+
+                var options = $.extend({}, BootstrapTable.defaultProps.options, options);
+
+                ReactDOM.render(
+                    React.createElement("div", null, 
+                        React.createElement(BootstrapTable, {data: output, striped: true, hover: true, search: true, exportCSV: true, options: options}, 
+                            React.createElement(TableHeaderColumn, {dataField: "name", isKey: true, dataAlign: "left", dataSort: true, dataFormat: link}, "Business Capability"), 
+                            React.createElement(TableHeaderColumn, {dataField: "count", width: "200", dataSort: true, dataFormat: criticalityFormat}, "# of apps"), 
+                            React.createElement(TableHeaderColumn, {dataField: "text", width: "200", dataSort: true, filter: {type: "TextFilter", placeholder: "Please enter a value"}}, "Average criticality")
+                        )
+                    ),
+                    document.getElementById("app")
+                );
+            });
+    };
+
+    return ReportApplicationsPerCapability;
+})();
 (function () {
     'use strict';
 
@@ -50807,9 +51094,16 @@ var ReportProjectDependencies = (function() {
         case 'data-quality-services':
             var report = new ReportDataQualityServices(reportSetup);
             break;
+        case 'responsibilities':
+            var report = new ReportResponsibilities(reportSetup);
+            break;
         case 'capability-spend':
-        default:
             var report = new ReportCapabilitySpend(reportSetup);
+            break;
+        case 'apps-per-capability':
+            var report = new ReportApplicationsPerCapability(reportSetup);
+            break;
+        default:
     }
 
     if (report)
